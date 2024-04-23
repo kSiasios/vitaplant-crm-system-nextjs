5a073b51ce01bb886ddfdd1e4811fb5ff70c1713
Konstantinos Siasios <kostassiasios1999@gmail.com>
2024-04-23 20:22:49 +0300

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="w-full flex gap-1 items-center justify-center py-4 absolute bottom-0">
+      <p>Copyright &copy; {new Date().getFullYear()}. Developed by</p>
+      <Link
+        href="https://www.linkedin.com/in/konstantinos-siasios/"
+        target="_blank"
+        className="underline hover:text-blue-500 focus:text-blue-500"
+      >
+        Konstantinos Siasios
+      </Link>
+    </footer>
+  );
+};
+
+export default Footer;
