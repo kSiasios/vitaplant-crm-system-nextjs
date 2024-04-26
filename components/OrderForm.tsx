c11450744cc1aa0435b1ba750a13405bc8eaf541
Konstantinos Siasios <kostassiasios1999@gmail.com>
2024-04-26 17:07:54 +0300

    Deleted a line

--- components/OrderForm.tsx
+++ components/OrderForm.tsx
@@ -65,12 +65,7 @@ const OrderForm = ({ handleSubmit, type }: OrderFormProps) => {
           />
         )}
       </div>
-
-      <input
-        className="px-3 py-2 rounded-lg border border-gray-400"
-        required={true}
-        placeholder="Payment Status"
-      />
+      <></>
       <button className="px-3 py-2 rounded-lg bg-white/40" type="submit">
         CLICK TO SUBMIT
       </button>
