34fb24ba9c9254ba488cbb2102c4131852853fdb
Konstantinos Siasios <kostassiasios1999@gmail.com>
2024-05-28 22:58:24 +0300

    Update OrderItemEntry.tsx

--- components/OrderItemEntry.tsx
+++ components/OrderItemEntry.tsx
@@ -433,7 +433,7 @@ const OrderItemEntry = ({
                               );
                             }}
                           />
-                          's
+                          &apos;s
                         </>
                       )}
                     </>
